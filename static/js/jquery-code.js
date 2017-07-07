480e0b6f3418fee5ab79a85c3bee0947d7e0e5ca
ganx <tedddy@foxmail.com>
2017-07-08 05:06:43 +0800

    ttst 增加jquery，并且使用显示和隐藏功能。采用的方法是学习mathjax的处理方法 ttst 删除google_analytics.html

---
+++ static/js/jquery-code.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#hide1").click(function(){
+        $("p1").hide();
+    });
+    $("#show1").click(function(){
+        $("p1").show();
+    });
+});
+
+$(document).ready(function(){
+    $("#hide2").click(function(){
+        $("p2").hide();
+    });
+    $("#show2").click(function(){
+        $("p2").show();
+    });
+});
