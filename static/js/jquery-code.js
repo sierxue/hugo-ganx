dc43bee7753d4ca4094045397ab543a48b30f993
ganx <tedddy@foxmail.com>
2017-07-08 17:04:41 +0800

    toggle显示和隐藏的jquery codes

--- static/js/jquery-code.js
+++ static/js/jquery-code.js
@@ -1,46 +1,58 @@
-$(document).ready(function(){
-    $("#hide1").click(function(){
-        $("hint1").hide();
-    });
-    $("#show1").click(function(){
-        $("hint1").show();
-    });
-});
+//ref: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+//ref: https://stackoverflow.com/questions/41268239/simple-hide-show-divjs-how-to-start-hidden
+function hint() {
+    var x = document.getElementById('hint');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
 
-$(document).ready(function(){
-    $("#hide2").click(function(){
-        $("hint2").hide();
-    });
-    $("#show2").click(function(){
-        $("hint2").show();
-    });
-});
-
-$(document).ready(function(){
-    $("#hide3").click(function(){
-        $("hint3").hide();
-    });
-    $("#show3").click(function(){
-        $("hint3").show();
-    });
-});
-
-
-$(document).ready(function(){
-    $("#hide4").click(function(){
-        $("hint4").hide();
-    });
-    $("#show4").click(function(){
-        $("hint4").show();
-    });
-});
-
-
-$(document).ready(function(){
-    $("#hide2").click(function(){
-        $("anwser").hide();
-    });
-    $("#show2").click(function(){
-        $("anwser").show();
-    });
-});
+//ref: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+// $(document).ready(function(){
+//     $("#hide1").click(function(){
+//         $("hint1").hide();
+//     });
+//     $("#show1").click(function(){
+//         $("hint1").show();
+//     });
+// });
+//
+// $(document).ready(function(){
+//     $("#hide2").click(function(){
+//         $("hint2").hide("slow");
+//     });
+//     $("#show2").click(function(){
+//         $("hint2").show("slow");
+//     });
+// });
+//
+// $(document).ready(function(){
+//     $("#hide3").click(function(){
+//         $("hint3").hide();
+//     });
+//     $("#show3").click(function(){
+//         $("hint3").show();
+//     });
+// });
+//
+//
+// $(document).ready(function(){
+//     $("#hide4").click(function(){
+//         $("hint4").hide();
+//     });
+//     $("#show4").click(function(){
+//         $("hint4").show();
+//     });
+// });
+//
+//
+// $(document).ready(function(){
+//     $("#hide5").click(function(){
+//         $("answer").hide();
+//     });
+//     $("#show5").click(function(){
+//         $("answer").show();
+//     });
+// });
